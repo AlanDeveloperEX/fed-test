bb7d769e9e1f5c9a423ebbd04bc3b76c848da402
Alan Santos <alan.santos.ex.contato@gmail.com>
2022-08-27 21:03:55 -0300

    fix: corrected line string and added an id to selector

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 var reportsWidget = {
     options: {
-        containerSelector: '.reports',
+        containerSelector: '#reports',
         template: (
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
