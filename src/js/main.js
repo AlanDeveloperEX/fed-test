59b73b7c5e57ea14ec1980947714174068ead140
Alan Santos <alan.santos.ex.contato@gmail.com>
2022-08-27 23:22:14 -0300

    refactor: better syntax to template span js

--- src/js/main.js
+++ src/js/main.js
@@ -12,9 +12,7 @@ var reportsWidget = {
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
-                                '<span> ({{file_size}}</span>' +
-                                '<span> {{file_type}})</span>' +
+                                '<a href="{{url}}" target="_blank">{{title}} <span>({{file_size}} {{file_type}})</span></a>' +
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
